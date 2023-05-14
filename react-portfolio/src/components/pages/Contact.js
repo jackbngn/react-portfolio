58af9ddae6b1abc5b4f3fb2de4e45b4b051ec20f
Jack <jackngn83@gmail.comk>
2023-05-13 23:55:55 -0700

    contact page started, added inputs fields, textarea, send button

--- react-portfolio/src/components/pages/Contact.js
+++ react-portfolio/src/components/pages/Contact.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import '../styles/contact.css';
 
 export default function Contact() {
 	return (
-		<div>
-			<h1>Contact Me</h1>
+		<div className="flex justify-center mt-12">
+			<form className="bg-gray-800">
+				<h1 className="text-white text-3xl font-bold flex justify-center">
+					Contact Me!
+				</h1>
+
+				<div className="m-8 flex">
+					<input className="h-8 w-64 mr-10 p-2" placeholder="Name" />
+					<input className="h-8 w-64 p-2" placeholder="Email" />
+				</div>
+
+				<div className="m-8">
+					<textarea className="h-32 w-full" placeholder="Message" />
+				</div>
+
+				<div className="flex justify-center">
+					<button className=" bg-blue-400 text-white font-bold rounded-xl p-2 ">
+						Send Message
+					</button>
+				</div>
+			</form>
 		</div>
 	);
 }
